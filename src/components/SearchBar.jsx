7327b398328bc47d80ddb19c33eb232c91f9b131
Joel Aliyu <joelaliyu1@gmail.com>
2024-03-14 17:22:49 +0000

    Added a search bar to the header

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 function SearchBar() {
   return (
-    <form className="w-[250px]">
+    <form className="w-[170px] sm:w-[250px]">
       <input
         className="w-full px-4 py-1 border-[1px] border-grey focus:border-1 focus:border-grey focus:outline-0 rounded-xl"
         type="text"
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,14 +2,17 @@ import SearchBar from "../components/SearchBar";
 
 function Header() {
   return (
-    <div className="col-start-2 flex justify-between items-center px-4">
+    <div className="col-start-2 flex justify-between items-center px-2 gap-5">
       <SearchBar />
-      <div className="">
+      <div className="flex gap-4 items-center">
         <img
           src="joel.JPG"
           alt="Profile pic"
           className="w-[40px] h-[40px] rounded-full object-center"
         />
+        <p className="hidden md:block text-md font-kanit text-black tracking-wider">
+          Joel Aliyu
+        </p>
       </div>
     </div>
   );
