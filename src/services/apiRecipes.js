08c591511ad443444c2d73b9e8d21d588ce668ad
Joel Aliyu <joelaliyu1@gmail.com>
2024-03-18 00:40:06 +0000

    Changed the number of popular recipes

--- src/services/apiRecipes.js
+++ src/services/apiRecipes.js
@@ -3,7 +3,7 @@ const apiKey = "1462b5c4536245548366d3a0a3b7ad74";
 
 export async function getPopularRecipes() {
   const res = await fetch(
-    `${baseURL}/recipes/random?number=9&apiKey=${apiKey}`
+    `${baseURL}/recipes/random?number=6&apiKey=${apiKey}`
   );
   if (!res.ok) throw new Error("Failed to fetch popular recipes");
 
